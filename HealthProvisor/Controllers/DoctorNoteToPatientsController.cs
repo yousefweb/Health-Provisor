6d6572161bc24e64cad3313ef6dfc96cbdaef10c
Yousef Aldeeb <129120120+yousefweb@users.noreply.github.com>
2024-05-12 23:50:18 +0300

    Update DoctorNoteToPatientsController.cs

--- HealthProvisor/Controllers/DoctorNoteToPatientsController.cs
+++ HealthProvisor/Controllers/DoctorNoteToPatientsController.cs
@@ -345,7 +345,7 @@ namespace HealthProvisor.Controllers
             {
                 smtp.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
                 smtp.Connect("smtp.gmail.com", 587, false);
-                smtp.Authenticate("HealthProvisor@gmail.com", "zfii pcdg ysyy aunn");
+                smtp.Authenticate("HealthProvisor@gmail.com", "");
                 await smtp.SendAsync(email);
                 smtp.Disconnect(true);
             }
